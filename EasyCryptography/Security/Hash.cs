765daf851b584fcbe0504c6bf727329605297a7f
huinalam <huinalam@gmail.com>
2014-05-01 05:17:02 +0900

    Update Hash.cs
    제안 합니당 :)
    
    메소드 하나로 사용이 가능하도록 만들었습니다. 비슷한 역활을 하는 메소드가 많아서요.
    소스 수정이 용이하게 만들었습니다. 즉, 하나를 변경하더라도 모두 적용되도록 구성하였습니당.

--- EasyCryptography/Security/Hash.cs
+++ EasyCryptography/Security/Hash.cs
@@ -5,26 +5,73 @@ using System.Text;
 
 namespace Variel.Security
 {
-    public static class Hash
+    /// <summary>
+    /// 사용할 해시 알고리즘을 선택합니다.
+    /// </summary>
+    public enum HashProvider
     {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA512
+    }
+
+    /// <summary>
+    /// string, Array byte, stream 값을 받아 해시 값으로 변환해줍니다.
+    /// </summary>
+    public static class HashService
+    {
+        /// <summary>
+        /// 알고리즘을 선택하여 값을 입력하면, 해시 알고리즘에 의해 나온 결과 값을 얻을 수 있습니다.
+        /// </summary>
+        /// <param name="hashProvider">적용하고자 하는 알고리즘</param>
+        /// <param name="inputString">변환하고자 하는 문자열</param>
+        /// <returns>해시 알고리즘에 의해 나온 결과 값을 반환합니다.</returns>
+        public static string GenerateHash(HashProvider hashProvider, string inputString)
+        {
+            return GenerateHash(hashProvider, Encoding.UTF8.GetBytes(inputString));
+        }
+
+        /// <summary>
+        /// 알고리즘을 선택하여 값을 입력하면, 해시 알고리즘에 의해 나온 결과 값을 얻을 수 있습니다.
+        /// </summary>
+        /// <param name="hashProvider">적용하고자 하는 알고리즘</param>
+        /// <param name="buffer">변환하고자 하는 바이트 배열</param>
+        /// <returns>해시 알고리즘에 의해 나온 결과 값을 반환합니다.</returns>
+        public static string GenerateHash(HashProvider hashProvider, byte[] buffer)
+        {
+            return GenerateHash(hashProvider, new MemoryStream(buffer));
+        }
+
+        /// <summary>
+        /// 알고리즘을 선택하여 값을 입력하면, 해시 알고리즘에 의해 나온 결과 값을 얻을 수 있습니다.
+        /// </summary>
+        /// <param name="hashProvider">적용하고자 하는 알고리즘</param>
+        /// <param name="inputStream">변환하고자 하는 Stream</param>
+        /// <returns>해시 알고리즘에 의해 나온 결과 값을 반환합니다.</returns>
+        public static string GenerateHash(HashProvider hashProvider, Stream inputStream)
+        {
+            switch (hashProvider)
+            {
+                case HashProvider.MD5:
+                    return MD5Hash(inputStream);
+
+                case HashProvider.SHA1:
+                    return SHA1Hash(inputStream);
+
+                case HashProvider.SHA256:
+                    return SHA256Hash(inputStream);
+
+                case HashProvider.SHA512:
+                    return SHA256Hash(inputStream);
+
+                default:
+                    throw new ArgumentNullException("HashProvider is null");
+            }
+        }
+
         #region MD5Hash
-        public static string MD5Hash(string input)
-        {
-            var inByte = Encoding.UTF8.GetBytes(input);
-            var hashed = MD5Hash(inByte);
-
-            return hashed;
-        }
-
-        public static string MD5Hash(byte[] input)
-        {
-            var sha = MD5CryptoServiceProvider.Create();
-            var hashed = sha.ComputeHash(input);
-
-            return BitConverter.ToString(hashed);
-        }
-
-        public static string MD5Hash(Stream input)
+        private static string MD5Hash(Stream input)
         {
             var sha = MD5CryptoServiceProvider.Create();
             var hashed = sha.ComputeHash(input);
@@ -34,23 +81,7 @@ namespace Variel.Security
         #endregion
 
         #region SHA1Hash
-        public static string SHA1Hash(string input)
-        {
-            var inByte = Encoding.UTF8.GetBytes(input);
-            var hashed = SHA1Hash(inByte);
-
-            return hashed;
-        }
-
-        public static string SHA1Hash(byte[] input)
-        {
-            var sha = SHA1CryptoServiceProvider.Create();
-            var hashed = sha.ComputeHash(input);
-
-            return BitConverter.ToString(hashed);
-        }
-
-        public static string SHA1Hash(Stream input)
+        private static string SHA1Hash(Stream input)
         {
             var sha = SHA1CryptoServiceProvider.Create();
             var hashed = sha.ComputeHash(input);
@@ -60,23 +91,7 @@ namespace Variel.Security
         #endregion
 
         #region SHA256Hash
-        public static string SHA256Hash(string input)
-        {
-            var inByte = Encoding.UTF8.GetBytes(input);
-            var hashed = SHA256Hash(inByte);
-
-            return hashed;
-        }
-
-        public static string SHA256Hash(byte[] input)
-        {
-            var sha = SHA256CryptoServiceProvider.Create();
-            var hashed = sha.ComputeHash(input);
-
-            return BitConverter.ToString(hashed);
-        }
-
-        public static string SHA256Hash(Stream input)
+        private static string SHA256Hash(Stream input)
         {
             var sha = SHA256CryptoServiceProvider.Create();
             var hashed = sha.ComputeHash(input);
@@ -86,23 +101,7 @@ namespace Variel.Security
         #endregion
 
         #region SHA512Hash
-        public static string SHA512Hash(string input)
-        {
-            var inByte = Encoding.UTF8.GetBytes(input);
-            var hashed = SHA512Hash(inByte);
-
-            return hashed;
-        }
-
-        public static string SHA512Hash(byte[] input)
-        {
-            var sha = SHA256CryptoServiceProvider.Create();
-            var hashed = sha.ComputeHash(input);
-
-            return BitConverter.ToString(hashed);
-        }
-
-        public static string SHA512Hash(Stream input)
+        private static string SHA512Hash(Stream input)
         {
             var sha = SHA256CryptoServiceProvider.Create();
             var hashed = sha.ComputeHash(input);
